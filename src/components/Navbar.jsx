b60432bafb2486e10cdc351392805e0f3f51426b
victoriaviv <victoriadufatanye2@gmail.com>
2025-04-20 23:09:45 +0200

    changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import "../styles/Navbar.css"
+import { FiMenu, FiX } from "react-icons/fi";
+import "../styles/Navbar.css";
 
-function Navbar(){
+function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
 
-return(
-<div className="navbar">
-<div className="logo">
-    <img src="Logo.png" alt="logo"/>
-</div>
-<div className="links">
-       
-      
-       <Link to="/">HOME</Link>
-       <Link to="/About">ABOUT US</Link>
-       <Link to="/Service">OUR SERVICE</Link>
-    <Link to="/Contact">CONTACT US</Link>
-     </div>
-</div>
-)
+  const toggleMenu = () => setMenuOpen(!menuOpen);
+  const closeMenu = () => setMenuOpen(false);
+
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <img src="Logo.png" alt="logo" />
+      </div>
+
+      <div className="menu-toggle" onClick={toggleMenu}>
+        {menuOpen ? <FiX /> : <FiMenu />}
+      </div>
+
+      <div className={`links ${menuOpen ? "open" : ""}`}>
+        <Link to="/" onClick={closeMenu}>HOME</Link>
+        <Link to="/About" onClick={closeMenu}>ABOUT US</Link>
+        <Link to="/Service" onClick={closeMenu}>OUR SERVICE</Link>
+        <Link to="/Contact" onClick={closeMenu}>CONTACT US</Link>
+      </div>
+    </div>
+  );
 }
+
 export default Navbar;
